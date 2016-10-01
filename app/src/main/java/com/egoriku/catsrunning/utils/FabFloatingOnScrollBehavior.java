c6fcc653b475670f7948ae9fc7ac8e44b422ceaa
egorikftp <K4TNeYj2>
2016-10-01 23:42:34 +0300

    new Behavoir

---
+++ app/src/main/java/com/egoriku/catsrunning/utils/FabFloatingOnScrollBehavior.java
@@ -0,0 +1,82 @@
+package com.egoriku.catsrunning.utils;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPropertyAnimatorListener;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Interpolator;
+
+public class FabFloatingOnScrollBehavior extends FloatingActionButton.Behavior {
+    private static final Interpolator INTERPOLATOR = new FastOutSlowInInterpolator();
+    private boolean mIsAnimatingOut = false;
+
+
+    public FabFloatingOnScrollBehavior(Context context, AttributeSet attrs) {
+        super();
+    }
+
+
+    @Override
+    public boolean onStartNestedScroll(final CoordinatorLayout coordinatorLayout, final FloatingActionButton child, final View directTargetChild, final View target, final int nestedScrollAxes) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL
+                || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+    }
+
+
+    @Override
+    public void onNestedScroll(final CoordinatorLayout coordinatorLayout, final FloatingActionButton child, final View target, final int dxConsumed, final int dyConsumed, final int dxUnconsumed, final int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+        if (dyConsumed > 0 && !this.mIsAnimatingOut && child.getVisibility() == View.VISIBLE) {
+            animateOut(child);
+        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+            animateIn(child);
+        }
+    }
+
+
+    private void animateOut(final FloatingActionButton button) {
+        if (Build.VERSION.SDK_INT >= 14) {
+            ViewCompat.animate(button).translationY(button.getHeight() + getMarginBottom(button)).setInterpolator(INTERPOLATOR).withLayer()
+                    .setListener(new ViewPropertyAnimatorListener() {
+                        public void onAnimationStart(View view) {
+                            mIsAnimatingOut = true;
+                        }
+
+                        public void onAnimationCancel(View view) {
+                            mIsAnimatingOut = false;
+                        }
+
+                        public void onAnimationEnd(View view) {
+                            mIsAnimatingOut = false;
+                            view.setVisibility(View.GONE);
+                        }
+                    }).start();
+        }
+    }
+
+
+    private void animateIn(FloatingActionButton button) {
+        button.setVisibility(View.VISIBLE);
+        if (Build.VERSION.SDK_INT >= 14) {
+            ViewCompat.animate(button).translationY(0)
+                    .setInterpolator(INTERPOLATOR).withLayer().setListener(null)
+                    .start();
+        }
+    }
+
+
+    private int getMarginBottom(View v) {
+        int marginBottom = 0;
+        final ViewGroup.LayoutParams layoutParams = v.getLayoutParams();
+        if (layoutParams instanceof ViewGroup.MarginLayoutParams) {
+            marginBottom = ((ViewGroup.MarginLayoutParams) layoutParams).bottomMargin;
+        }
+        return marginBottom;
+    }
+}
