ac0efb87bb6fa67e0f0702916b8ff36a3d27238e
egorikU <egorikftp@gmail.com>
2017-05-14 18:01:13 +0300

    master: remove scripts

--- scripts/build.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-GRADLE="./gradlew"
-$GRADLE assembleRelease
-RELEASEFILE="${CIRCLE_ARTIFACTS}/fitnesscats-release-${CIRCLE_BUILD_NUM}-${CIRCLE_SHA1}.apk"
-cp ./app/build/outputs/apk/app-release.apk ${RELEASEFILE}
