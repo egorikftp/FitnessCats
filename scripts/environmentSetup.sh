3a12c55f49b642120f0f215c9ff9bcece2075a23
egorikU <egorikftp@gmail.com>
2017-05-14 16:49:08 +0300

    master: update script

--- scripts/environmentSetup.sh
+++ scripts/environmentSetup.sh
@@ -13,7 +13,7 @@ function getAndroidSDK(){
     echo y | android update sdk -u -a -t android-25 &&
     echo y | android update sdk -u -a -t extra-android-m2repository &&
     echo y | android update sdk -u -a -t extra-android-support &&
-    echo y | android update sdk -u -a -t build-tools-25.1.0 &&
+    echo y | android update sdk -u -a -t build-tools-25.0.2 &&
     echo y | android update sdk -u -a -t sys-img-armeabi-v7a-android-18 &&
     ##echo no | android create avd -n testAVD -f -t android-19 --abi default/x86 &&
     touch $DEPS
